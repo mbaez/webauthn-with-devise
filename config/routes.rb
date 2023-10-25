907ecd0b6491b69a75a1304c437e25348dfce3a7
Maximiliano Báez González <mxbg.py@gmail.com>
2023-10-25 11:58:07 -0300

    Add base resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   root to: "dashboard#index"
 
-  devise_for :users
+  namespace :webauthn do
+    resources :credentials, only: %i[index create destroy] do
+      collection do
+        resource :challenge, only: %i[create], module: :credentials, as: :credentials_challenge
+      end
+    end
+
+    resources :authentications, only: %i[index create], path: :auth do
+      collection do
+        resource :challenge, only: %i[create], module: :authentications, as: :authentications_challenge
+      end
+    end
+  end
+
+  devise_for :users, controllers: {sessions: "users/sessions"}
 end
